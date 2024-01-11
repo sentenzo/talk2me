ad828b461a27b07b6ddada7ba883d50257bba157
sentenzo <sentenzo@users.noreply.github.com>
2024-01-11 18:19:58 +0300

    experimenting with real-time mode

--- talk2me/__main__.py
+++ talk2me/__main__.py
@@ -1,7 +1,10 @@
+import time
+
 from .utils import (
     audio_to_text,
     mic_to_text,
     mic_to_text_background,
+    mic_to_text_streem,
     print_mic_list,
 )
 
@@ -12,4 +15,8 @@ if __name__ == "__main__":
     # time.sleep(10)
     # stop()
 
-    print(mic_to_text(5))
+    # print(mic_to_text(5))
+
+    for text in mic_to_text_streem():
+        print(text, end="\r")
+    print()
--- talk2me/utils.py
+++ talk2me/utils.py
@@ -1,9 +1,14 @@
+import time
+from queue import Queue
+
 import speech_recognition as sr
+from speech_recognition.audio import AudioData
 
 # https://github.com/Uberi/speech_recognition/blob/master/examples/audio_transcribe.py
 
 
 SR_RECOGNIZER = sr.Recognizer()
+DEFAULT_MODEL = "tiny.en"  # tiny, base, small, medium, large
 
 
 def audio_to_text(path_to_audio, cached_recognizer=SR_RECOGNIZER) -> str:
@@ -11,8 +16,38 @@ def audio_to_text(path_to_audio, cached_recognizer=SR_RECOGNIZER) -> str:
     r = cached_recognizer
     with sr.AudioFile(path_to_audio) as source:
         audio = r.record(source)  # read the entire audio file
-    text = r.recognize_whisper(audio, model="base")
+    text = r.recognize_whisper(audio, model=DEFAULT_MODEL)
     return text
+
+
+def mic_to_text_streem(mic_index=None, cached_recognizer=SR_RECOGNIZER):
+    print("Say something!")
+    r = cached_recognizer
+    que_audio = Queue()
+    acc_audio = None
+
+    def record_callback(_, audio: AudioData) -> None:
+        # data = audio.get_raw_data()
+        que_audio.put(audio)
+
+    source = sr.Microphone(mic_index)
+
+    stop_listening = r.listen_in_background(source, record_callback, 1)
+    try:
+        while True:
+            time.sleep(0.5)
+            if que_audio.empty():
+                continue
+
+            audio = que_audio.get()
+
+            if acc_audio:
+                acc_audio.frame_data += audio.frame_data
+            else:
+                acc_audio = audio
+            yield r.recognize_whisper(acc_audio, model=DEFAULT_MODEL)
+    except KeyboardInterrupt:
+        stop_listening(False)
 
 
 def print_mic_list():
@@ -29,7 +64,7 @@ def mic_to_text(
     with sr.Microphone(mic_index) as source:
         print("Say something!")
         audio = r.listen(source, 5, time_limit)
-    return r.recognize_whisper(audio, model="base")
+    return r.recognize_whisper(audio, model=DEFAULT_MODEL)
 
 
 def mic_to_text_background(
@@ -42,7 +77,7 @@ def mic_to_text_background(
 
     def callback(recognizer, audio):
         print("###")
-        print(recognizer.recognize_whisper(audio, model="base"))
+        print(recognizer.recognize_whisper(audio, model=DEFAULT_MODEL))
 
     stop_listening = r.listen_in_background(
         m, callback, phrase_time_limit=time_limit
